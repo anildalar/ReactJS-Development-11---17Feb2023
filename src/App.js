b3a810d0c1286de8598387de4ab752092f75866c
unknown <dollorinfotech@gmail.com>
2023-02-17 09:36:45 +0530

    v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Import Statement
+import React from 'react'
+
+// import {someNamedImport1,someNamedImport2,....} from 'somelibary';
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+export default function App() {
+    // Every function rreturn something
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={ <Home /> }></Route>
+                <Route path="/login" element={<Login/>}></Route>
+                <Route path="/register" element={<Register />}></Route>
+            </Routes>
+        </Router>
+    )
 }
-
-export default App;
